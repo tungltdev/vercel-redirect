cd2989bf29b4432c5389a30e8c5efa8626a73049
Lê Thanh Tùng <31556693+tungltdev@users.noreply.github.com>
2023-06-16 16:15:01 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -5,8 +5,6 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-	//const graphQLClient = new GraphQLClient(endpoint);
-	//const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
 	const path = pathArr.join('/');
 	//console.log(path);
@@ -17,57 +15,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			destination: `${
 				endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
 			}`,
-		},
-	};
-	
-	// redirect if facebook is the referer or request contains fbclid
-	if (1) {
-	//if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
-	}
-	const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
-
-	const data = await graphQLClient.request(query);
-	if (!data.post) {
-		return {
-			notFound: true,
-		};
-	}
-	return {
-		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
 		},
 	};
 };
